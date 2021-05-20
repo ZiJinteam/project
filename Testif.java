a76564f5f87ae2c047a44892807c8ff4c5b20d61
MatsuSakura <lexingtonqy@outlook.com>
2021-05-20 21:11:42 +0800

    创建 Testif.java

---
+++ Testif.java
@@ -0,0 +1,22 @@
+public class Testif{
+    LOOP:
+        for(char c='a';c<='z';c++){
+            switch(c){
+                case 'a': continue;
+                case 'z':
+                case 'm': break LOOP;
+            }
+            System.out.print(c);
+        }
+        System.out.println("");
+        long sum=0,i=1,max=1931918,N=0;
+        while(true){
+            sum=sum+i;
+            if(sum>max){
+                N=i-1;
+                break;
+            }
+            i++;
+        }
+        System.out.println("满足1+2+...+n<="+max+"的最大整数n为"+N);
+}
