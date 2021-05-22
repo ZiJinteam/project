914157dbcf56a6927c828cf27e40ddd69a82aa52
MatsuSakura <lexingtonqy@outlook.com>
2021-05-22 15:17:21 +0800

    dbconnectest

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var Connection = require('tedious').Connection
+var Request = require('tedious').Request
+
+var config = {
+  server: 'localhost',
+  authentication: {
+    type: 'default',
+    options: {
+      userName: 'sa', // update me
+      password: '123' // update me
+    }
+  }
+}
+var connection = new Connection(config)
+
+connection.on('connect', function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    executeStatement()
+  }
+})
+
+function executeStatement () {
+  request = new Request("select 123, 'hello world'", function (err, rowCount) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(rowCount + ' rows')
+    }
+    connection.close()
+  })
+
+  request.on('row', function (columns) {
+    columns.forEach(function (column) {
+      if (column.value === null) {
+        console.log('NULL')
+      } else {
+        console.log(column.value)
+      }
+    })
+  })
+
+  connection.execSql(request)
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -14,5 +59,6 @@ router.delete('/', function (req, res) {
 router.use(function(req,res,next){
   console.log('time',Date.now());
   next();
-})
+});
+
 module.exports = router;
