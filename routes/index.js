88ca766c6597a8d934e0b72dbfc5e3baa2504d45
MatsuSakura <lexingtonqy@outlook.com>
2021-05-22 12:47:06 +0800

    route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/',function(req,res){
-  res.send("hello,world");
+router.get('/home',function(req,res){
+  res.send("GET request to the homepage");
 });
-router.post('/', function (req, res) {
-  res.send('Got a POST request');
+router.post('/home', function (req, res) {
+  res.send('POST request to the homepage');
 });
+router.route('/book')
+  .get(function(req, res) {
+    res.send('Get a random book');
+  })
+  .post(function(req, res) {
+    res.send('Add a book');
+  })
+  .put(function(req, res) {
+    res.send('Update the book');
+  });
+
 module.exports = router;
