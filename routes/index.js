5aa165b4ee22214c0d6498c0294c2326f1d1405d
MatsuSakura <lexingtonqy@outlook.com>
2021-05-22 11:34:55 +0800

    test

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/',function(req,res){
+  res.send("hello,world");
+});
+router.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+module.exports = router;
