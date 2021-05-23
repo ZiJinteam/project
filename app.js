80631634d7a4f04fc214e05bf1c2b3721370840f
MatsuSakura <lexingtonqy@outlook.com>
2021-05-23 14:35:38 +0800

    111

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/birds', birdsRouter);
+app.use('/index', indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,5 +40,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -52,79 +52,20 @@ router.get('/users/:id', function (req, res, next) {
     res.end(req.params.id);
   });
 
-  var http =require('http');
-  var url=require('url');
-  var fs=require('fs');
-  // 解析传来的数据
-  var querystring=require('querystring');
-   
-  var server=http.createServer();
-   
-  var ser=server.listen(6500)
-   
-  //获取文件路径
-  var basePath=__dirname;
-  var loginPath=basePath+'/html'+'/web/index.html';
-  var mainPath=basePath+'/html'+'/web/inquire.html';
-   
-  // 监听请求
-  server.on('request',function(req,res)
-  {
-      // 有请求就打印
-      console.log("有请求");
-      //解析url
-      var urlString=url.parse(req.url);
-      //根据不同的URL来处理不同的请求
-      switch(urlString.pathname)
-      {
-          case '/inquire':
-              sendData(mainPath,req,res);
-              break;
-          case '/login/check':
-              requestData(req,res); 
-              break;
-          case '/login':
-              sendData(loginPath,req,res);
-              break;
-          default:
-              res.writeHead(404,'err',{'content-type':'text/html;charset=utf-8'});
-              res.write("出错了");
-              res.end();
-              break;
-      };
-   
-      function sendData(file,req,res)
-      {
-          fs.readFile(file,function(err,data)
-          {
-              if(err)
-              {
-                  res.writeHead(404,'err',{'content-type':'text/html;charset=utf-8'});
-                  res.write("出错辽");
-                  res.end();
-              }else
-              {
-                  res.writeHead(200,'o98k',{'content-type':'text/html;charset=utf-8'});
-                  res.write(data);
-                  res.end();
-              }
-          });
-      };
-      function requestData(req,res)
-      {
-          res.writeHead(200,'oj8k',{'content-type':'text/html;charset=utf-8'});
-              // console.log(req.method);
-              // console.log(urlString);
-              // console.log(querystring.parse(urlString.query));
-              if(req.method.toUpperCase()=="POST")
-              {
-                  var str=""; 
-                  //从缓冲区读取数据
-                  req.on('data',function(da){str+=da;});
-                  // req.on('end',function(){console.log(querystring.parse(str))});
-                  req.on('end',function(){res.end(str);});
-              }
-      }
+  app.get("/web/login.html", function(request, response) {
+    fs.readFile("./"+request.path.substr(1),function(err,data){
+         // body
+         if(err){
+             console.log(err);
+             //404：NOT FOUND
+             response.writeHead(404,{"Content-Type":"text/html"});
+         }
+         else{
+             //200：OK
+             response.writeHead(200,{"Content-Type":"text/html"});
+             response.write(data.toString());
+         }
+         response.end();
+     });
   });
-  
 module.exports = router;
